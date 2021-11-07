ed9ea520fc74ba2cf3b524a2e03bf457e91e24c1
mihaylovski <90508736+mihaylovski@users.noreply.github.com>
2021-11-07 17:28:02 +0100

    Finished SVG chapter

--- JavaScript/10. Web APIs/webapis.js
+++ JavaScript/10. Web APIs/webapis.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+// 01. Introduction
+// Web API: Aplication Programing Interface
+
+// 02. Web History API
+// Provides easy methods to access the window.history object 
+
+// 03. Web Storage API
+// Is a simple syntax for storing and retriving date in the browser.
+
+// 04. Web Worker API
+// A web worker is a JS running in the background, without affecting the performance of the page.
+
+// 05. Web Fetch API
+// The Fetch API interface allows web browser to make HTTP request to the web server
+
+// 06. Web Geolocation API
+// The HTML Geolocation API is used to get the geographical positions of a user.
